6276a994d85a2cba2777e66f398443aec917ce41
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-02-24 19:58:53 +0200

    upload

---
+++ calculate.js
@@ -0,0 +1,56 @@
+//islem isaretini lutfen string deger olarak girelim!!
+
+function hesapMakinesi(sayi1, sayi2, islemIsareti){
+    
+    if( isNaN(sayi1) || isNaN(sayi2 ))console.log('string deger girdiniz lutfen number giriniz!!');
+    else{
+        if ( islemIsareti == '+'){
+            let sonuc = {
+              islemSonucu:  sayi1+sayi2,
+              secim: 'toplama'
+    
+                     }
+               return sonuc;
+            } 
+        
+        else if ( islemIsareti == '-'){
+            let sonuc = {
+              islemSonucu:  sayi1-sayi2,
+              secim: 'cikarma'
+    
+                     }
+                return sonuc;
+            }
+        
+        else if ( islemIsareti == '*'){
+            let sonuc = {
+              islemSonucu:  sayi1*sayi2,
+              secim: 'carpma'
+    
+                     }
+                 return sonuc;
+            }
+        
+        else if ( islemisareti == '/'){
+       
+                 if(sayi2==0) console.log('ikinci sayi sifira esit.Bu yuzden sonuc belirsizdir');
+                 else {
+                    let sonuc = {
+                      islemsonucu:  sayi1/sayi2,
+                      secim: 'bolme'
+            
+                             }
+                 return sonuc;
+            }
+          }
+    
+        else console.log('boyle bir islem bulunamadi!!');
+    }
+    
+    }
+    
+      const islemm = hesapMakinesi(74,  4,'+');
+      console.log(islemm);
+    
+    //gelistirilecektir!!
+    
